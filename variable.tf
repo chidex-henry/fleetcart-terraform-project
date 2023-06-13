89fcb2aa2ca14d6a4ce9123e7a3cc82a6547ecdb
Chidi Ukaegbu <chidex.ukaegbu4real@gmail.com>
2023-06-13 01:28:56 -0500

    created security group file

--- variable.tf
+++ variable.tf
@@ -61,3 +61,11 @@ variable "private_data_subnet_az2_cidr" {
     type           = string
 
 }
+
+#security group variable 
+variable "ssh_location" {
+    default        = "0.0.0.0/0"
+    description    = "the ip address that can ssh into the ec2 intances"
+    type           = string
+
+}
