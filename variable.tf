2dc9ec9ae53ce65c03fdcba0b1da1d5bc020babb
Chidi Ukaegbu <chidex.ukaegbu4real@gmail.com>
2023-06-22 00:55:19 -0500

    created route53 file

--- variable.tf
+++ variable.tf
@@ -153,4 +153,20 @@ variable "ec2_key_pair_name" {
 
 }
 
+# Route 53 variables 
+variable "domain_name" {
+    default        = "chidiukaegbu.com"
+    description    = "domain name"
+    type           = string
+
+}
+
+variable "record_name" {
+    default        = "www"
+    description    = "sub domain name"
+    type           = string
+
+}
+
+
 
